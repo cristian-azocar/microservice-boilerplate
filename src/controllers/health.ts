1d9bbb1f8c06084cce9063db464ee3b9f94cbf3f
Cristian Azócar <caam.1429@gmail.com>
2020-04-09 11:34:32 -0400

    Change type of ctx parameter

--- src/controllers/health.ts
+++ src/controllers/health.ts
@@ -1,10 +1,10 @@
-import { ParameterizedContext } from 'koa';
+import { Context } from 'koa';
 import HealthService from '../services/health';
 
 export default class HealthController {
   healthService: HealthService = new HealthService();
 
-  async getHealthInfo(ctx: ParameterizedContext): Promise<void> {
+  async getHealthInfo(ctx: Context): Promise<void> {
     ctx.body = await this.healthService.getHealthInfo();
   }
 }
