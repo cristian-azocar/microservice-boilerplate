d78e62c05e6bca4f274e63a027439eb448c0a6c5
Cristian Azócar <caam.1429@gmail.com>
2020-04-09 11:39:20 -0400

    Add coverage tests configuration

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   roots: ['<rootDir>/__tests__'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  preset: 'ts-jest',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 };
