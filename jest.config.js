821681abf25eab782fc7a4558b28c5ff9932c676
Cristian Azócar <caam.1429@gmail.com>
2020-04-15 13:20:19 -0400

    Add module mappers to jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@ module.exports = {
   roots: ['<rootDir>/__tests__'],
   preset: 'ts-jest',
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  moduleNameMapper: {
+    'src/(.*)': '<rootDir>/src/$1',
+    settings: '<rootDir>/settings',
+    '__matches__/(.*)': '<rootDir>/__matches__/$1',
+    '__mocks__/(.*)': '<rootDir>/__mocks__/$1',
+  },
 };
