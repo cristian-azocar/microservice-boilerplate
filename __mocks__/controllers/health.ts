6986ddd55db505c6a150106b0dcbbfa4fe0c90e4
Cristian Azócar <caam.1429@gmail.com>
2020-04-15 12:41:35 -0400

    Add health controller mock

---
+++ __mocks__/controllers/health.ts
@@ -0,0 +1,30 @@
+import { Context } from 'koa';
+import HealthController from '../../src/controllers/health';
+
+const getSpy = (): jest.SpyInstance =>
+  jest
+    .spyOn(HealthController.prototype, 'getHealthInfo')
+    .mockImplementation((ctx: Context) => {
+      ctx.body = {
+        nodeVersion: 'v12.14.1',
+        service: 'MS Test Boilerplate',
+        memory: {
+          rss: 51957760,
+          heapTotal: 29876224,
+          heapUsed: 10250344,
+          external: 1294527,
+          arrayBuffers: 0,
+        },
+        pid: 44617,
+        uptime: 2.651281754,
+        environment: 'development',
+        appPackage: 'ms-boilerplate',
+        appVersionPackage: '1.0.0',
+      };
+
+      return Promise.resolve();
+    });
+
+export default {
+  getSpy,
+};
