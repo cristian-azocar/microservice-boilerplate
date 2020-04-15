c30a6c76b7528309fd77e8737af1150191afd0d2
Cristian Azócar <caam.1429@gmail.com>
2020-04-15 12:42:13 -0400

    Add health controller mock to test

--- __tests__/routes/health.test.ts
+++ __tests__/routes/health.test.ts
@@ -1,10 +1,23 @@
 import supertest, { SuperTest, Test } from 'supertest';
 import app from '../../src/app';
 import healthMatch from '../../__matches__/health';
+import healthControllerMock from '../../__mocks__/controllers/health';
+
+jest.mock('../../src/controllers/health');
 
 const appTest: SuperTest<Test> = supertest(app.callback());
 
 describe('health router', (): void => {
+  let spy: jest.SpyInstance;
+
+  beforeAll((): void => {
+    spy = healthControllerMock.getSpy();
+  });
+
+  afterAll((): void => {
+    spy.mockRestore();
+  });
+
   it('should return a JSON with the health info', async (): Promise<void> => {
     const response = await appTest.get('/health').expect(200);
 
