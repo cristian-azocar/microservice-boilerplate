983e3dd8f529f271e0ff8360de900647e51cbae6
Cristian Azócar <caam.1429@gmail.com>
2020-04-16 12:14:46 -0400

    Add bodyparser and login-router

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
 import HealthRouter from 'src/routes/health';
+import LoginRouter from 'src/routes/login';
 
 const app: Koa = new Koa();
 const healthRouter: HealthRouter = new HealthRouter();
+const loginRouter: LoginRouter = new LoginRouter();
 
+app.use(bodyParser());
 app.use(healthRouter.getRoutes());
+app.use(loginRouter.getRoutes());
 
 export default app;
