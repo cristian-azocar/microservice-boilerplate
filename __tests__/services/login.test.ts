857d68c9b94663151edbc1333685f867f6773695
Cristian Azócar <caam.1429@gmail.com>
2020-04-20 12:22:35 -0400

    Add login service tests

---
+++ __tests__/services/login.test.ts
@@ -0,0 +1,15 @@
+import LoginService from 'src/services/login';
+import LoginResponse from 'src/models/responses/login';
+import loginSchema from '__tests__/schemas/login';
+
+describe('login service', (): void => {
+  it('should return the login information', async (): Promise<void> => {
+    const loginService: LoginService = new LoginService();
+    const loginResponse: LoginResponse = await loginService.login(
+      'john.doe',
+      'secretpassword'
+    );
+
+    expect(loginResponse).toMatchObject(loginSchema);
+  });
+});
