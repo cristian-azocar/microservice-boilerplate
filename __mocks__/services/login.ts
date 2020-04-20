fc298c63f4a67f303d043cecddd1918d349588cb
Cristian Azócar <caam.1429@gmail.com>
2020-04-20 11:47:40 -0400

    Complete login tests

--- __mocks__/services/login.ts
+++ __mocks__/services/login.ts
@@ -1,17 +1,29 @@
 import LoginService from 'src/services/login';
 
-const getSpy = (): jest.SpyInstance =>
+export interface MockOptions {
+  simulateUserNotFound: boolean;
+}
+
+const defaultMockOptions: MockOptions = {
+  simulateUserNotFound: false,
+};
+
+const getSpy = (options: MockOptions = defaultMockOptions): jest.SpyInstance =>
   jest
     .spyOn(LoginService.prototype, 'login')
-    .mockImplementation((username: string) =>
-      Promise.resolve({
+    .mockImplementation((username: string) => {
+      if (options.simulateUserNotFound) {
+        return Promise.resolve(null);
+      }
+
+      return Promise.resolve({
         username,
         name: 'John',
         lastName: 'Doe',
         email: 'john.doe@fake.com',
         createdAt: new Date(),
-      })
-    );
+      });
+    });
 
 export default {
   getSpy,
--- __tests__/controllers/login.test.ts
+++ __tests__/controllers/login.test.ts
@@ -7,15 +7,17 @@ import loginServiceMock from '__mocks__/services/login';
 describe('login controller', (): void => {
   let spy: jest.SpyInstance;
 
-  beforeAll((): void => {
-    spy = loginServiceMock.getSpy();
-  });
+  // beforeAll((): void => {
+  //   spy = loginServiceMock.getSpy();
+  // });
 
   afterAll((): void => {
     spy.mockRestore();
   });
 
-  it('should set ctx body with login information', async (): Promise<void> => {
+  it('should set ctx.body with login information', async (): Promise<void> => {
+    spy = loginServiceMock.getSpy();
+
     const loginController: LoginController = new LoginController();
     const ctx: Context = createMockContext();
 
@@ -28,5 +30,27 @@ describe('login controller', (): void => {
 
     expect(ctx.body).toMatchObject(loginSchema);
     expect(ctx.body.username).toEqual(ctx.request.body.username);
+  });
+
+  it('should set ctx.body with an error object and ctx.status with a 404 code when credentials are invalid', async (): Promise<
+    void
+  > => {
+    spy = loginServiceMock.getSpy({ simulateUserNotFound: true });
+
+    const loginController: LoginController = new LoginController();
+    const ctx: Context = createMockContext();
+
+    ctx.request.body = {
+      username: 'john.doe',
+      password: 'secretpassword',
+    };
+
+    await loginController.login(ctx);
+
+    expect(ctx.status).toEqual(404);
+    expect(ctx.body).toMatchObject({
+      code: expect.any(Number),
+      message: expect.any(String),
+    });
   });
 });
