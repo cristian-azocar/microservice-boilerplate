8eb20ebf372205c9bf76c06d0c42229fc43a95d2
Cristian Azócar <caam.1429@gmail.com>
2020-04-21 15:00:01 -0400

    Add koa-logger

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
+import logger from 'koa-logger';
 import HealthRouter from 'src/routes/health';
 import LoginRouter from 'src/routes/login';
 
@@ -7,6 +8,7 @@ const app: Koa = new Koa();
 const healthRouter: HealthRouter = new HealthRouter();
 const loginRouter: LoginRouter = new LoginRouter();
 
+app.use(logger());
 app.use(bodyParser());
 app.use(healthRouter.getRoutes());
 app.use(loginRouter.getRoutes());
