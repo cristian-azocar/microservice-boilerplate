992e3a4865835515951a72e2b329545779878578
Cristian Azócar <caam.1429@gmail.com>
2020-04-27 15:51:10 -0400

    Add module to import all node_module mocks

--- tests/cases/src/utils/api-docs.test.ts
+++ tests/cases/src/utils/api-docs.test.ts
@@ -1,7 +1,4 @@
-import 'tests/mocks/node-modules/koa2-swagger-ui';
-import 'tests/mocks/node-modules/fs';
-import 'tests/mocks/node-modules/js-yaml';
-import 'tests/mocks/node-modules/json-refs';
+import 'tests/mocks/node-modules/mock-all';
 import { Middleware } from 'koa';
 import ApiDocsUtils from 'src/utils/api-docs';
 
