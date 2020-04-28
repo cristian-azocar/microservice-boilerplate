86f8e62923950676a6f8a84911899ba59bf3191f
Cristian Azócar <caam.1429@gmail.com>
2020-04-28 12:40:06 -0400

    Update error model test

--- tests/cases/src/models/responses/error.test.ts
+++ tests/cases/src/models/responses/error.test.ts
@@ -1,8 +1,10 @@
 import ErrorResponse from 'src/models/responses/error';
 
-it('should set default values when invoked with falsey parameters', (): void => {
-  const errorResponse: ErrorResponse = new ErrorResponse(null);
+describe('error model', (): void => {
+  it('should set default values when invoked with falsey parameters', (): void => {
+    const errorResponse: ErrorResponse = new ErrorResponse(null);
 
-  expect(errorResponse.code).toEqual(500);
-  expect(errorResponse.message).toEqual('Internal Server Error');
+    expect(errorResponse.code).toEqual(500);
+    expect(errorResponse.message).toEqual('Internal Server Error');
+  });
 });
