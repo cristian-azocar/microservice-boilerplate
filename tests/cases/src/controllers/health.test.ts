a8b106585f8f5d142b5594e60e9b8131cf2e5b84
Cristian Azócar <caam.1429@gmail.com>
2020-04-28 12:05:16 -0400

    Update health and login controller tests

--- tests/cases/src/controllers/health.test.ts
+++ tests/cases/src/controllers/health.test.ts
@@ -1,18 +1,12 @@
+import 'tests/mocks/services/health';
 import { Context } from 'koa';
 import { createMockContext } from '@shopify/jest-koa-mocks';
 import HealthController from 'src/controllers/health';
 import healthInfoMatch from 'tests/schemas/health';
-import healthServiceMock from 'tests/mocks/services/health';
 
 describe('health controllers', (): void => {
-  let spy: jest.SpyInstance;
-
-  beforeAll((): void => {
-    spy = healthServiceMock.getSpy();
-  });
-
   afterAll((): void => {
-    spy.mockRestore();
+    jest.resetAllMocks();
   });
 
   it('should return the health information', async (): Promise<void> => {
