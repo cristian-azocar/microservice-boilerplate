7886056e52526272d9a66cf2528405d291ef18c0
Cristian Azócar <caam.1429@gmail.com>
2020-05-05 11:20:12 -0400

    Throw another intentional error to test the pipeline

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ const port: number = +process.env.PORT || 3000;
 
 // Intentional error to test the pipeline
 throw new Error('Error to test the pipeline');
+throw new Error('Error 2 to test the pipeline');
 
 app.listen(port, (): void => {
   // eslint-disable-next-line no-console
