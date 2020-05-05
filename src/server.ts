5baade761a31fcd4d150f248c8af9e0eaab960c6
Cristian Azócar <caam.1429@gmail.com>
2020-05-05 11:35:00 -0400

    Remove intentionals errors

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,6 @@ import app from 'src/app';
 
 const port: number = +process.env.PORT || 3000;
 
-// Intentional error to test the pipeline
-throw new Error('Error to test the pipeline');
-throw new Error('Error 2 to test the pipeline');
-
 app.listen(port, (): void => {
   // eslint-disable-next-line no-console
   console.log(`Server running in port ${port}`);
