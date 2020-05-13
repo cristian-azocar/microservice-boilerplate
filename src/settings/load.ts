d5c44a78121549029d2dca477cb498c283e3a222
Cristian Azócar <caam.1429@gmail.com>
2020-05-13 12:56:39 -0400

    Add support to read config from local.yml file

--- src/settings/load.ts
+++ src/settings/load.ts
@@ -4,29 +4,33 @@ import yaml from 'js-yaml';
 
 const NODE_ENV: string = process.env.NODE_ENV || 'development';
 const settingsPath = '../../settings';
+const format = {
+  parse: yaml.safeLoad,
+  stringify: yaml.safeDump,
+};
 
 // Default values have first priority
 nconf.defaults({
   NODE_ENV,
 });
 
-// Command line args and environment vars have second priority
+// Local file have second priority
+nconf.file('local', {
+  file: path.join(__dirname, `${settingsPath}/local.yml`),
+  format,
+});
+
+// Command line args and environment vars have third priority
 nconf.argv().env();
 
-// Files based on environment have third priority
-nconf.file({
+// Files based on environment have fourth priority
+nconf.file('environment', {
   file: path.join(__dirname, `${settingsPath}/${NODE_ENV}.yml`),
-  format: {
-    parse: yaml.safeLoad,
-    stringify: yaml.safeDump,
-  },
+  format,
 });
 
 // Default file have the least priority
-nconf.file({
+nconf.file('default', {
   file: path.join(__dirname, `${settingsPath}/default.yml`),
-  format: {
-    parse: yaml.safeLoad,
-    stringify: yaml.safeDump,
-  },
+  format,
 });
