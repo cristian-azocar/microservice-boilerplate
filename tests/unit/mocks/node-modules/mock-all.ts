f0fd3efba33b5efcb6611938ac742e481e707cd6
Cristian Azócar <caam.1429@gmail.com>
2020-05-14 16:19:42 -0400

    Isolate settings tests

--- tests/unit/mocks/node-modules/mock-all.ts
+++ tests/unit/mocks/node-modules/mock-all.ts
@@ -2,3 +2,5 @@ import 'tests/unit/mocks/node-modules/koa2-swagger-ui';
 import 'tests/unit/mocks/node-modules/fs';
 import 'tests/unit/mocks/node-modules/js-yaml';
 import 'tests/unit/mocks/node-modules/json-refs';
+import 'tests/unit/mocks/node-modules/path';
+import 'tests/unit/mocks/node-modules/nconf';
