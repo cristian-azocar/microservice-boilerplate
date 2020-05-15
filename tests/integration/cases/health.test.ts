cae468b225b3d20dd0bcce5c71dbb2911ef12f26
Cristian Azócar <caam.1429@gmail.com>
2020-05-15 11:21:59 -0400

    Create health test

--- tests/integration/cases/health.test.ts
+++ tests/integration/cases/health.test.ts
@@ -1,13 +1,15 @@
 import supertest, { Response, SuperTest, Test } from 'supertest';
 import nconf from 'nconf';
+import healthSchema from 'tests/schemas/health';
 
-describe('content', () => {
+describe('health', (): void => {
   const server: SuperTest<Test> = supertest(nconf.get('APP_URL'));
 
-  it('should return 200 and correct content-type', async (): Promise<void> => {
+  it('should return the health of the service', async (): Promise<void> => {
     const response: Response = await server.get('/health');
 
     expect(response.status).toEqual(200);
     expect(response.type).toEqual('application/json');
+    expect(response.body).toMatchObject(healthSchema);
   });
 });
