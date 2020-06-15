f4ca1fda0c3aef72ad32675ad68fca8d35bf9341
Cristian Azócar <caam.1429@gmail.com>
2020-06-15 12:50:13 -0400

    Update tests

--- tests/schemas/login.ts
+++ tests/schemas/login.ts
@@ -3,7 +3,7 @@ import 'tests/unit/matchers/to-be-types';
 export default {
   username: expect.any(String),
   name: expect.any(String),
-  lastName: expect.any(String),
   email: expect.any(String),
-  createdAt: expect.toBeTypes([Date, String]),
+  creationDate: expect.toBeTypes([Date, String]),
+  token: expect.any(String),
 };
